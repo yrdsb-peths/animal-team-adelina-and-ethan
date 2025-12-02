089d1006c878e0e6de6b623aa2226b20b6c8ee70
EEEEMoji <ethanyeungsikyuen@gmail.com>
2025-12-02 14:31:16 -0500

    Added sound

---
+++ Elephant.java
@@ -0,0 +1,40 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * elephant, our hero
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Elephant extends Actor
+{
+    GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
+    
+    /**
+     * Act - do whatever the Elephant wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        if (Greenfoot.isKeyDown("left")) {
+            move(-1);
+        } else if (Greenfoot.isKeyDown("right")) {
+            move(1);
+        }
+        
+        // remove apple is elephant eats it
+        eat();
+    }
+    
+    public void eat() {
+        if (isTouching(Apple.class)) {
+            removeTouching (Apple.class);
+            MyWorld world = (MyWorld) getWorld();
+            world.createApple();
+            world.increaseScore();
+            elephantSound.play();
+        }
+        
+    }
+}
--- MyWorld.java
+++ MyWorld.java
@@ -2,6 +2,47 @@ import greenfoot.*;
 
 public class MyWorld extends World {
     public MyWorld() {
+<<<<<<< Updated upstream
         super(600, 400, 1);
+=======
+        //setImage("background.jpg");
+        
+        super(600, 400, 1, false);
+        
+        //create the elephant object
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 300);
+        
+        //create a label
+        scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 50, 50);
+        
+        createApple();
+    }
+
+    /**
+     * end the game and draw "GameOver"
+     */
+    public void gameOver() {
+        Label gameOverLabel = new Label ("Game Over", 100);
+        addObject(gameOverLabel, 300, 200);
+    }
+    
+    /**
+     * increase score
+     */
+    public void increaseScore() {
+        score ++;
+        scoreLabel.setValue(score);
+    }
+    
+    /**
+     * create a new apple at random location at top of screen
+     */
+    public void createApple() {
+        Apple apple= new Apple();
+        int x = Greenfoot.getRandomNumber(600);
+        addObject(apple, x, 0);
+>>>>>>> Stashed changes
     }
 }
