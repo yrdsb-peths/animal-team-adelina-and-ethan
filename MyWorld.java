bdd2f2a6746b32f514960c877ebb23bd75597792
EEEEMoji <ethanyeungsikyuen@gmail.com>
2025-12-02 14:38:40 -0500

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -2,11 +2,9 @@ import greenfoot.*;
 
 public class MyWorld extends World {
     public MyWorld() {
-<<<<<<< Updated upstream
 
         super(600, 400, 1);
-=======
->>>>>>> Stashed changes
+
         //setImage("background.jpg");
         
         super(600, 400, 1, false);
