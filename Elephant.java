7ea9f41ce4f668586f63a71368289688d8d668fe
adelinalyw <adelina.lyw@gmail.com>
2025-12-03 13:33:45 -0500

    added image to constructor

--- Elephant.java
+++ Elephant.java
@@ -12,6 +12,9 @@ public class Elephant extends Actor
      * Act - do whatever the Elephant wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public Elephant() {
+        setImage("images/elephant.png");
+    }
     public void act()
     {
         // Add your action code here.
