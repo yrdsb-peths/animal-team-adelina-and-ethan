214c5598204927a32b5d71d7349dc8e96321fbd8
EEEEMoji <ethanyeungsikyuen@gmail.com>
2025-12-04 14:13:34 -0500

    added title screen

---
+++ TitleScreen.java
@@ -0,0 +1,69 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TitleScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TitleScreen extends World
+{
+    Label titleLabel = new Label("Hungry Elephant", 60);
+    /**
+     * Constructor for objects of class TitleScreen.
+     * 
+     */
+    public TitleScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1);
+        
+        addObject(titleLabel, getWidth()/2, 100);
+        prepare();
+    }
+    
+    /**
+     * The main world act loop
+     */
+    
+    public void act(){
+        if(Greenfoot.isKeyDown("space")){
+            MyWorld gameWorld = new MyWorld();
+            Greenfoot.setWorld(gameWorld);
+            
+        }
+    }
+    
+    /**
+     * Prepare the world for the start of the program
+     * That is: create an initial object and add them to the world
+     */
+    private void prepare(){
+        Elephant elephant = new Elephant();
+        addObject(elephant,479, 87);
+        elephant.setLocation(494,120);
+        elephant.setLocation(494,120);
+        Label label = new Label("Use \u2190 and \u2192 to Move", 40);
+        addObject(label,297,120);
+        label.setLocation(100,194);
+        //label.setLocation(333,229);
+
+        Label label2 = new Label("Press <space> to Start", 40);
+        addObject(label2,240,289);
+        label2.setLocation(261,285);
+        label2.setLocation(266,281);
+        label.setLocation(223,227);
+        elephant.setLocation(550,91);
+        label.setLocation(221,227);
+        label.setLocation(276,234);
+        label.setLocation(260,231);
+        label.setLocation(260,231);
+        label.setLocation(275,220);
+        label.setLocation(298,229);
+        label.setLocation(307,229);
+        label.setLocation(300,225);
+        label2.setLocation(313,282);
+        label2.setLocation(300,275);
+        label2.setLocation(300,275);
+    }
+}
